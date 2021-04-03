26b01f8fc3c2f47c01277a12bd422c34f63f9f1c
Donelle Harris <donelle.harris@gmail.com>
2021-04-02 22:38:19 -0500

    Create initial files for project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MyPantry.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
